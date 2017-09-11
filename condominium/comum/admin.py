35515ffc45e3524bf1e7ad11a69101ce2c3c5af1
Rogério Silva <rogerio410@users.noreply.github.com>
2017-09-11 20:54:24 -0300

    Criação dos modelos de Perfil, UninidadeHabitacional, GrupoHabitacional, implementação do Admin.

--- condominium/comum/admin.py
+++ condominium/comum/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from .models import Condominio
+from .models import Condominio, Perfil, GrupoHabitacional, UnidadeHabitacional
 
 
 @admin.register(Condominio)
 class CondominioAdmin(admin.ModelAdmin):
     list_display = ('cnpj', 'nome', 'endereco', )
+
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('sexo', 'telefone', 'data_nascimento', 'foto', 'unidade_habitacional', 'usuario', )
+
+
+@admin.register(GrupoHabitacional)
+class GrupoHabitacionalAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'tipo_unidade', 'nome', 'logradouro', 'condominio', )
+
+
+@admin.register(UnidadeHabitacional)
+class UnidadadeHabitacionalAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'grupo_habitacional', 'proprietario', )
+
+
--- condominium/comum/models.py
+++ condominium/comum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 """
 Sobre organizaçaõ de Código e PEP-8: utilizar o Python
@@ -11,6 +12,7 @@ classes, semanticamente organizadas
  > business methods
  
 """
+
 
 class Base(models.Model):
 
@@ -29,3 +31,59 @@ class Condominio(Base):
         verbose_name_plural = 'Condominios'
         #unique_together = (('attr1', 'attr2'), )
         ordering = ('nome', )
+
+
+class GrupoHabitacional(Base):
+
+    TIPO_GRUPO_HABITACIONAL = (('quadra', u'Quadra'), ('bloco', u'Bloco'), ('torre', u'Torre'))
+    TIPO_UNIDADE_HABITACIONAL = (('apartamento', u'Apartamento'), ('casa', u'Casa'), ('chale', u'Chale'))
+
+    tipo = models.CharField('Tipo', choices=TIPO_GRUPO_HABITACIONAL, blank=False, null=False)
+    tipo_unidade = models.CharField('Tipo unidade' ,choices=TIPO_UNIDADE_HABITACIONAL, blank=False, null=False)
+    nome = models.CharField('Nome' ,max_length=50, unique=True, blank=False, null=False)
+    logradouro = models.CharField('logradouro' ,max_length=200, blank=False, null=False)
+    condominio = models.ForeignKey(Condominio, null=False, related_name='condominio')
+
+    class Meta:
+        verbose_name = 'Grupo Habitacional'
+        verbose_name_plural = 'Grupos Habitacionais'
+        ordering = ('nome', )
+
+
+class UnidadeHabitacional(Base):
+
+    nome = models.CharField(max_length=20, unique=True, blank=False, null=False)
+    grupo_habitacional = models.ForeignKey(GrupoHabitacional, null=False, related_name='grupo_habitacional')
+    proprietario = models.ForeignKey(Perfil, blank=False, null=False, related_name='proprietario')
+
+    class Meta:
+        verbose_name = 'Unidade habitacional'
+        verbose_name_plural = 'Unidades habitacionais'
+        ordering = ('nome', )
+
+
+class Perfil(Base):
+
+    SEXO = (('M', u'Masculino'), ('F', u'Feminino'))
+
+    sexo = models.CharField('Sexo', choices=SEXO, blank=False, null=False)
+    telefone = models.CharField('Telefone', max_length=20, blank=False, null=False)
+    data_nascimento = models.DateTimeField('Data de nascimento', blank=False, null=False)
+    foto = models.CharField(max_length=200)
+    unidade_habitacional = models.ForeignKey(UnidadeHabitacional, blank=False, null=False, related_name= 'moradores')
+
+    usuario = models.OneToOneField(User, related_name='perfil')
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
+        #ordering = ('usuario.username')
+
+
+
+
+
+
+
+
+
