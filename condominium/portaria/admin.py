ac4101e3c3455c93072374c5c3c3418667f3d680
HipolitoJr <hipolitocavalcantejr@gmail.com>
2017-09-30 16:46:45 -0300

    Atualização para Inline Model do Comentario para ComentarioOcorrencia, de portaria no admin

--- condominium/portaria/admin.py
+++ condominium/portaria/admin.py
@@ -2,14 +2,22 @@ from django.contrib import admin
 from portaria.models import Ocorrencia, Entrada, Comentario
 
 
+class ComentarioOcorreniaInline(admin.TabularInline):
+    model = Comentario
+    fields = ('descricao', )
+    extra = 1
+
+
 @admin.register(Ocorrencia)
 class OcorrenciaAdmin(admin.ModelAdmin):
-    list_display = ('status', 'descricao', 'informante')
+    list_display = ('status', 'descricao', 'informante', )
+
+    inlines = (ComentarioOcorreniaInline, )
 
 
 @admin.register(Entrada)
 class EntradaAdmin(admin.ModelAdmin):
-    list_display = ('data', 'hora', 'descricao', 'informante')
+    list_display = ('data', 'hora', 'descricao', 'informante', )
 
     fieldsets = (
         (None, {
@@ -17,12 +25,7 @@ class EntradaAdmin(admin.ModelAdmin):
         }),
     )
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.informante = request.user.perfil
-        obj.save()
-
-
-@admin.register(Comentario)
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ('descricao', 'ocorrencia')
+    def save_model(self, request, entrada, form, change):
+        if not entrada.pk and not request.user.is_superuser:
+            entrada.informante = request.user.perfil
+        entrada.save()
