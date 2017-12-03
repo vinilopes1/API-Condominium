af11802579b5e67931e2556b2d3a7684087b784e
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2017-12-03 09:08:41 -0300

    Dependences and settings for PG.

--- condominium/condominium/settings.py
+++ condominium/condominium/settings.py
@@ -81,10 +81,18 @@ WSGI_APPLICATION = 'condominium.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'sqlite': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'condominium',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+   }
 }
 
 
