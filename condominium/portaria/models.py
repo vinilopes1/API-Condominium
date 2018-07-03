45681419c6170b04e20b43a33b29ac3bf907b066
hipolito <hipolitocavalcantejr@gmail.com>
2018-07-03 20:48:01 -0300

    Implementação dos Models adicionais:  > Post  > Visitante  > Aviso  e mudanças em relações entre classes.

--- condominium/portaria/models.py
+++ condominium/portaria/models.py
@@ -2,6 +2,18 @@
 from django.db import models
 from comum.models import Base, Perfil
 
+
+class Post(Base):
+
+    descricao = models.CharField('Descricao', max_length=256, blank=True, null=True)
+    foto = models.CharField('Foto', max_length=256, blank=True, null=True)
+
+    class Meta:
+
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
 class Ocorrencia(Base):
 
     STATUS_OCORRENCIA = (
@@ -17,6 +29,8 @@ class Ocorrencia(Base):
     localizacao = models.CharField('Localizacao', max_length=128, blank=True, null=True)
     publico = models.BooleanField('Publico', default=False, blank=False, null=False)
     informante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='ocorrencias', blank=False, null=False)
+
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
 
     class Meta:
 
@@ -57,6 +71,8 @@ class Entrada(Base):
     informante = models.ForeignKey(Perfil, on_delete = models.CASCADE, related_name = 'entradas', blank=False, null=False)
     status = models.CharField('Status', max_length=64, choices=STATUS_ENTRADA, default='informada', blank=False, null=False)
 
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+
     class Meta:
 
         verbose_name = 'Entrada'
@@ -64,3 +80,46 @@ class Entrada(Base):
 
     def __str__(self):
         return self.descricao
+
+
+class Aviso(Base):
+
+    PRIORIDADE_AVISO = (
+        ('baixa', 'Baixa'),
+        ('razoavel', 'Razoavel'),
+        ('urgente', 'Urgente'),
+    )
+
+    descricao = models.CharField('Descricao', max_length=256, blank=False, null=False)
+    publico = models.BooleanField('Publico', default=False, blank=False, null=False)
+    prioridade = models.CharField('Prioridade', choices=PRIORIDADE_AVISO, default='razoavel', max_length=256, blank=False, null=False)
+
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+
+    class Meta:
+
+        verbose_name = 'Aviso'
+        verbose_name_plural = 'Avisos'
+
+    def __str__(self):
+        return self.descricao
+
+
+class Visitante(Base):
+
+    SEXO_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    )
+
+    nome = models.CharField('Nome', max_length=256, blank=False, null=False)
+    sexo = models.CharField('Sexo', max_length=16, choices=SEXO_CHOICES, blank=False, null=False)
+    telefone = models.CharField('Telefone', max_length=16, blank=False, null=False)
+    data_nascimento = models.DateField('Data de nascimento', blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Visitante'
+        verbose_name_plural = 'Visitantes'
+
+    def __str__(self):
+        return self.nome
