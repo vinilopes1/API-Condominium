f259c70c40f2594f90c8ced0db7bbed0551aefdb
hipolito <hipolitocavalcantejr@gmail.com>
2018-07-23 20:43:51 -0300

    Configuração dos 'STATICFILES_DIR'

--- condominium/condominium/settings.py
+++ condominium/condominium/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'condominium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,6 +83,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/templates/static/',
 ]
 
 WSGI_APPLICATION = 'condominium.wsgi.application'
