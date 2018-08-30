64ae65f46b010d5f9c3397f5faa603e4062b12c7
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-08-30 18:40:13 -0300

    Configuração arquivos do heroku

--- condominium/wsgi.py
+++ condominium/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for api-condominium project.
+WSGI config for condominium project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api-condominium.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "condominium.settings")
 
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
