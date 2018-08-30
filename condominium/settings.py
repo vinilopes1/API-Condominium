18a732f7714401b6484f98d62d4c5e2b175e0a1f
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-08-30 18:44:27 -0300

    Configuring the app

--- condominium/settings.py
+++ condominium/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'condomais.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'api-condomais.herokuapp.com']
 
 
 # Application definition
